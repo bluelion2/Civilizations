b43f5968ca4df7f5a51fa268c746aa29172455b7
bluelion2 <iltk2000@gmail.com>
2019-03-01 15:44:08 +0900

    init

---
+++ src/main/maps.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './maps';
+
+
+export default class Maps extends Component {
+    render() {
+        return(
+            <div className="maps-container">
+                Map Component
+            </div>
+        );
+    }
+
+}
