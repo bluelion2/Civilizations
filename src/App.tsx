62b88b3a56fc5ac6a07212f8ed145994c5c15c7e
bluelion2 <iltk2000@gmail.com>
2019-03-03 14:04:53 +0900

    get click latlng

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,59 @@
 import React, { Component } from 'react';
 import './App.scss';
-import Maps from './main/maps';
-declare let geocoder: any;
-declare let google: any;
+
+declare var google: any;
 declare let window: any;
 declare let map: any;
+
 class App extends Component {
 
   state = {
     location: '',
   }
 
-  componentDidMount() {
+  // google map variable
+  map: any;
+  geocoder: any;
+  googleMapsPromise: any = false;
+
+  async componentDidMount() {
+    await this.setScript();
+  }
+
+  setScript() {
     const script = document.createElement('script');
     script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBD0xjTlohJCtVepzm6Up7-hQWmsZvU6uk';
+    script.async = true;
     document.head.appendChild(script);
   }
 
   // 검색해서 해당 주소 찾기
   find() {
-    const geocoder =  new google.maps.Geocoder();
-    const map = new google.maps.Map(document.getElementById('map'), {
+    const geocoder = new google.maps.Geocoder();
+    this.map = new google.maps.Map(document.getElementById('map'), {
       zoom: 10,
-      center: {lat: 126.97546671970849, lng: 37.5745879197085} // 광화문 기본 셋팅
+      // center: {lat: this.getRandomeMapPoint(), lng: this.getRandomeMapPoint()}
+      // center: {lat: 126.97546671970849, lng: 37.5745879197085} // 광화문 기본 셋팅
     });
 
-    geocoder.geocode({ 'address': this.state.location},
+    geocoder.geocode({'address': this.state.location},
       (results: any, status: any) => {
       if (status == 'OK') {
         // console.log(map, 'map function');
         // console.log('===');
         // console.log(results[0].geometry)
-        
-        console.log(new google.maps.LatLng())
         // x, y 좌표 catch
-        console.log('lat', results[0].geometry.viewport.ga.j);
-        console.log('lng', results[0].geometry.viewport.ma.j);
-        map.setCenter(results[0].geometry.location);
-        const marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
-        });
+        console.log('lng', results[0].geometry.viewport.ga.j);
+        console.log('lat', results[0].geometry.viewport.ma.j);
+        this.map.setCenter(results[0].geometry.location);
+        // const marker = new google.maps.Marker({
+        //   map: this.map,
+        //   position: results[0].geometry.location
+        // });
       } else {
         console.log('Geocode was not successful for the following reason: ' + status);
       }
     });
-
-    // this.setState({
-    //   location: '',
-    // })
   }
 
   input(event: any) {
@@ -57,8 +62,19 @@ class App extends Component {
     })
   }
 
+  getRandomeMapPoint(): number {
+    return Math.round((Math.random()*360 - 180) * 1000)/1000;
+  }
+
+  // map click 이벤트 : 좌표 catch
   mapClick() {
-    console.log('a')
+    const google = window.google;
+    google.maps.event.addListener(this.map, 'click', (e: any) => {
+      // console.log(e.latLng, this.map);
+      // console.log('===')
+      // 클릭 위치 좌표
+      console.log(e.latLng.lat(), e.latLng.lng())
+    });
   }
 
   render(): JSX.Element {
@@ -68,7 +84,6 @@ class App extends Component {
           <input type="text" placeholder="find location" onChange={() => this.input(event)}></input>
           <button onClick={() => this.find()}>Get location</button>
         </div>
-        {/* <Maps lat={0} lan={1}></Maps> */}
         <div className="map" id="map" onClick={() => this.mapClick()}>
 
         </div>
